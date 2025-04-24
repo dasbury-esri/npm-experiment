9ead7cf9d42b771207bd663aa821dde69e05c5fc
David Asbury <DAsbury@esri.com>
2025-04-24 12:30:11 -0700

    added MDN example repo

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,8 @@
 import js from "@eslint/js";
 import globals from "globals";
+import reactRecommended from "eslint-plugin-react/configs/recommended.js";
+import reactJSXRuntime from "eslint-plugin-react/configs/jsx-runtime.js";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
 import pluginReact from "eslint-plugin-react";
 import markdown from "@eslint/markdown";
 import css from "@eslint/css";
@@ -7,9 +10,18 @@ import { defineConfig } from "eslint/config";
 
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
+  { ignores: ["node_modules", "dist"],},
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], settings: { react: { version: "detect",},}, },
   { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
   pluginReact.configs.flat.recommended,
   { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
   { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+  reactRecommended,
+  reactJSXRuntime,
+  {
+    plugins: {
+      "react-hooks": reactHooksPlugin,
+    },
+    rules: reactHooksPlugin.configs.recommended.rules,
+  },
 ]);
